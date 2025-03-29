1cfaa7f61b24b3deb1ca828130dd2e17048b3edf
Rudra <goutam.prasad.2707@gmail.com>
2025-03-29 20:59:47 +0530

    add music tracks and cover images to the project

---
+++ assets/musicList.js
@@ -0,0 +1,49 @@
+import song1 from "./songs/DieWithASmile.mp3";
+import song2 from "./songs/Blue.mp3";
+import song3 from "./songs/CheriCheriLady.mp3";
+import song4 from "./songs/Espresso.mp3";
+import song5 from "./songs/WeDontTalkAnymore.mp3";
+
+import cover1 from "./covers/DieWithASmile.jpg";
+import cover2 from "./covers/Blue.jpg";
+import cover3 from "./covers/CheriCheriLady.jpg";
+import cover4 from "./covers/Espresso.jpg";
+import cover5 from "./covers/WeDontTalkAnymore.jpg";
+
+const musicList = [
+	{
+		id: 1,
+		title: "Die With A Smile",
+		artist: "Lady Gaga, Bruno Mars",
+		audio: song1,
+		cover: cover1,
+	},
+	{
+		id: 2,
+		title: "Blue",
+		artist: "Yung Kai",
+		audio: song2,
+		cover: cover2,
+	},
+	{
+		id: 3,
+		title: "Cheri Cheri Lady",
+		artist: "Modern Talking",
+		audio: song3,
+		cover: cover3,
+	},
+	{
+		id: 4,
+		title: "Espresso",
+		artist: "Sabrina Carpenter",
+		audio: song4,
+		cover: cover4,
+	},
+	{
+		id: 5,
+		title: "We Don't Talk Anymore",
+		artist: "Charlie Puth, Selena Gomez",
+		audio: song5,
+		cover: cover5,
+	},
+];
